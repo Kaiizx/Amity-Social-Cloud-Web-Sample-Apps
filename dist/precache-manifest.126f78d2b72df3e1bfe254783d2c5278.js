3397dbad26d3a4f66d2724de875834c587d1679e
ATIKAN PAYUNGTHONG <64010956@KMITL.AC.TH>
2023-03-16 22:55:25 +0700

    first commit

---
+++ dist/precache-manifest.126f78d2b72df3e1bfe254783d2c5278.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed3d65bf44a96cf6f71",
+    "url": "/css/app.6e1f8f14.css"
+  },
+  {
+    "revision": "0d9755c91d02295aee198293430d34e8",
+    "url": "/img/logo.0d9755c9.png"
+  },
+  {
+    "revision": "78cbe4e36cdac6e3b0932f864b925b71",
+    "url": "/img/spinner_dark_fast.78cbe4e3.gif"
+  },
+  {
+    "revision": "2950a0707634cc4b93be9848c57e846f",
+    "url": "/img/spinner_dark_slow.2950a070.gif"
+  },
+  {
+    "revision": "73240a5f3fd205f3138c52947974276a",
+    "url": "/img/spinner_fast.73240a5f.gif"
+  },
+  {
+    "revision": "d6cafba6c94b3aa32b2471f59b23603d",
+    "url": "/img/spinner_slow.d6cafba6.gif"
+  },
+  {
+    "revision": "edd5281093a815843b0ba9848dea3656",
+    "url": "/img/user_placeholder.edd52810.png"
+  },
+  {
+    "revision": "ed85052b7d5ebe270a134d4f07cea535",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ed3d65bf44a96cf6f71",
+    "url": "/js/app.76d7180d.js"
+  },
+  {
+    "revision": "f110edd7a71c0e0a206d",
+    "url": "/js/chunk-vendors.497926d8.js"
+  },
+  {
+    "revision": "811e20e5d7c7dc92ab76383c9196655b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
